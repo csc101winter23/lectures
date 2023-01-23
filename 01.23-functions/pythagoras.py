da6149fea0fc34c973030802ea8d188f5c032956
cesiu <cesiu@calpoly.edu>
2023-01-23 07:25:53 -0800

    Added base example of function use cases.

---
+++ 01.23-functions/pythagoras.py
@@ -0,0 +1,4 @@
+side_a = int(input("How long is one leg? "))
+side_b = int(input("How long is one leg? "))
+side_c = (side_a ** 2 + side_b ** 2) ** 0.5
+print("The hypotenuse has length " + str(side_c))
