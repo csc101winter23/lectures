50f7295fd29224beb6d51f6230862136b8bed0c4
cesiu <cesiu@calpoly.edu>
2023-02-22 13:25:02 -0800

    Added base example for dict problems.

---
+++ 02.22-dictionary-problems/words.py
@@ -0,0 +1,14 @@
+import string
+
+
+def sanitize_string()
+
+
+def main():
+    # Each word will be mapped to its frequency by a dictionary -- this is
+    #  better than a list, because it allows us to use the words as the keys,
+    #  to lookup information using words instead of searching for them.
+
+
+if __name__ == "__main__":
+    main()
---
+++ 02.22-dictionary-problems/words_tests.py
@@ -0,0 +1,10 @@
+import unittest
+import words
+
+
+class TestWords(unittest.TestCase):
+    def test01_(self):
+
+
+if __name__ == "__main__":
+    unittest.main()
